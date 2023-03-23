7801f6124865527119c6f3cb49754dcc97b53300
Alex Trimper <trimper.a@gmail.com>
2023-03-23 15:59:00 -0300

    changed root to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pages/home'
+  root to: 'pages#home'
   get 'pages/contact'
   get 'pages/music'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
