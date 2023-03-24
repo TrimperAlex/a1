4a536245b86dcff1bde4e564d04327064fc36c6e
Alex Trimper <trimper.a@gmail.com>
2023-03-24 11:02:01 -0300

    generated projects controller with scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  resources :projects
   get 'pages/contact'
   get 'pages/music'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
